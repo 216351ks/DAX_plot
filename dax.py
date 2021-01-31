91c8b947b7040c620ab9debe213bd45608016c16
wegrzynmaciej <wegrzyn.maciej@tlen.pl>
2021-01-31 12:46:44 +0100

    Update

--- dax.py
+++ dax.py
@@ -5,37 +5,36 @@ Created on Sun Jan 31 10:49:29 2021
 @author: Hubert
 """
 
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 
-tab = pd.read_csv("dax.csv" ''', use_columns = ['Data','Otwarcie','Zamkniecie']''')
+tab = pd.read_csv("./dax.csv", usecols=['Otwarcie', 'Data'], delimiter=';', header=0)
+
 
 def select_rows():
-    
-    for row in tab.readlines(): 
-        array = row.split(",")
+    for index, row in tab.iterrows():
+        # array = row.split(",")
         selected_rows = (row['Otwarcie'], row['Data'])
-        return selected_rows
-    
+    return selected_rows
+
+
 def draw_plot():
-    
     x = []
-    y= []
-    
-    for line in tab:
-        x.append(int(line[0]))
-        y.append(int(line[0]))
-        
-    plt.plot(x,y, label = 'Dax_price')
-    plt.xlabel('Cena')
-    plt.ylabel('Data')
-    
+    y = []
+
+    for index, line in tab.iterrows():
+        x.append(line[0])
+        y.append(line[1])
+
+    plt.plot(x, y, label='Dax_price')
+    plt.xlabel('Data')
+    plt.ylabel('Cena')
+
     plt.title('Cena Daxa')
     plt.legend()
-    
+
     plt.show()
-    
-    
+
+
 print(select_rows())
 print(draw_plot())
-
