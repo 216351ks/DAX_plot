61090c05f9d546b870dc351dfa71fa887aff32f5
216351ks <kacpersiwiec00@interia.pl>
2021-02-01 21:40:23 +0100

    update

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def start_page():
-    return render_template('index.html')
+    return render_template('start.html')
 
 @app.route('/login')
 def user_login():
-    return render_template('login.html')
+    return render_template('login.html', title = 'Login')
 
+@app.route('/contact')
+def contac_page():
+    return render_template('contact.html', title = 'Contact')
 
 if __name__ == "__main__":
     app.run(debug=True)
