ff1ca7d6cfcfe9878c69e9a21a70408775b0ca37
216351ks <kacpersiwiec00@interia.pl>
2021-02-01 22:37:32 +0100

    update

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '34d7823c19b2e8ff040e750cd571fd71'
+
 
 @app.route('/')
 def start_page():
     return render_template('start.html')
 
-@app.route('/login')
-def user_login():
-    return render_template('login.html', title = 'Login')
 
-@app.route('/contact')
-def contac_page():
-    return render_template('contact.html', title = 'Contact')
+@app.route('/register')
+def registration():
+    registration_form = RegistrationForm()
+    return render_template('login.html', title='Register', form=registration_form)
+
+
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', title='Login', form=login_form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
